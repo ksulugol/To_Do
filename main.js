088c8354a0a713c91b523918bc0b087d5083b68f
ksulugol <lugol.k8@gmail.com>
2022-02-04 10:29:06 +0300

    to do on objects (after stream)

--- main.js
+++ main.js
@@ -12,65 +12,44 @@ const list = {
  "write a post": STATUS.IN_PROGRESS,
 }
 
-function deleteTask(task) {
-  if (task in list) {
-    delete list[task];
+function deleteTask(name) {
+  if (name in list) {
+    delete list[name];
   }
 }
 
-function addTask(task) {
-  list[task] = DEFAULT_STATUS;
+function addTask(name) {
+  list[name] = DEFAULT_STATUS;
 }
 
-function changeStatus(task, status) {
-  if (task in list) {
-    list[task] = status;
+function changeStatus(name, status) {
+  if (name in list) {
+    list[name] = status;
   }
 }
 
-function showList () {
-  let count = 0;
+function showList() {
+  const tasks = {
+    [STATUS.TO_DO]: '',
+    [STATUS.IN_PROGRESS]: '',
+    [STATUS.DONE]: '',
+  }
 
-  console.log ('To Do:');
-  for (task in list) {
-    if (list[task] === STATUS.TO_DO) {
-      console.log (` '${[task]}'`);
-      count++;
-    }
+  for (let name in list) {
+    tasks[list[name]] += ` '${name}'` + '\n';
   }
-  if (count === 0) {
-    console.log (' -');
-  }
-  count = 0;
 
-  console.log ('In Progress:');
-  for (task in list) {
-    if (list[task] === STATUS.IN_PROGRESS) {
-      console.log (` '${[task]}'`);
-      count++;
-    }
-  }
-  if (count === 0) {
-    console.log (' -');
-  }
-  count = 0;
-
-  console.log ('Done:');
-  for (task in list) {
-    if (list[task] === STATUS.DONE) {
-      console.log (` '${[task]}'`);
-      count++;
-    }
-  }
-  if (count === 0) {
-    console.log (' -');
-  }
-  count = 0;
+  console.log('To do: \n' + (tasks[STATUS.TO_DO] || ' -') +
+            '\nIn progress: \n' + (tasks[STATUS.IN_PROGRESS] || ' -') +
+            '\nDone: \n' + (tasks[STATUS.DONE] || ' -'));
 }
 
 
-// console.log (addTask('have a walk') );
-// console.log (deleteTask('make a bed'));
-// console.log (changeStatus('write a post', 'Done'));
-console.log(showList());
+// addTask('have a walk') ;
+// deleteTask('make a bed');
+// changeStatus('write a post', 'Done');
+showList();
 // console.log (list);
+
+``
+
