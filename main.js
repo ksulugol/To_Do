d667cf680f2ff4218aef2007b4dd7088e1f71752
ksulugol <lugol.k8@gmail.com>
2022-02-05 22:09:21 +0300

    todo array

--- main.js
+++ main.js
@@ -1,55 +1,118 @@
+const list = [ ]
+
 const STATUS = {
   TO_DO: 'To Do',
   IN_PROGRESS: 'In Progress',
   DONE: 'Done',
 }
 
-const DEFAULT_STATUS = STATUS.TO_DO;
-
-const list = {
- "create a new practice task": STATUS.TO_DO,
- "make a bed": STATUS.TO_DO,
- "write a post": STATUS.IN_PROGRESS,
+const PRIORITY = {
+  LOW: 'low',
+  HIGH: 'high',
 }
 
-function deleteTask(name) {
-  if (name in list) {
-    delete list[name];
-  }
+function addTask(task) {
+  list.push({
+    name: task,
+    status: STATUS.TO_DO,
+    priority: PRIORITY.LOW,
+  })
 }
 
-function addTask(name) {
-  list[name] = DEFAULT_STATUS;
+function deleteTask(task) {
+  let findTask = list.findIndex(item => item.name = task);
+  list.splice(findTask, 1);
 }
 
-function changeStatus(name, status) {
-  if (name in list) {
-    list[name] = status;
-  }
+function changeStatus(task, status) {
+  list.filter( function (item) {
+    if (item.name === task) {
+      return item.status = status;
+    }
+  })
+}
+
+function changePriority(task, priority) {
+  list.filter( function(item) {
+    if (item.name === task) {
+      return item.priority = priority;
+    }
+  })
 }
 
 function showList() {
-  const tasks = {
-    [STATUS.TO_DO]: '',
-    [STATUS.IN_PROGRESS]: '',
-    [STATUS.DONE]: '',
-  }
 
-  for (let name in list) {
-    tasks[list[name]] += ` '${name}'` + '\n';
-  }
+  for (let key in STATUS) {
+    console.log(STATUS[key] + ':');
+    let StatusFilter = list.filter(item => item.status === STATUS[key]);
+    console.log(StatusFilter);
 
-  console.log('To do: \n' + (tasks[STATUS.TO_DO] || ' -') +
-            '\nIn progress: \n' + (tasks[STATUS.IN_PROGRESS] || ' -') +
-            '\nDone: \n' + (tasks[STATUS.DONE] || ' -'));
+    if (StatusFilter.length === 0 ) {
+      console.log(' -');
+    }
+  }
 }
 
-
-// addTask('have a walk') ;
-// deleteTask('make a bed');
-// changeStatus('write a post', 'Done');
-showList();
+addTask('have a walk');
+addTask('make a dinner');
+addTask('write a post');
+// deleteTask('have a walk');
+changeStatus('write a post', STATUS.DONE);
+changePriority('make a dinner', PRIORITY.HIGH );
 // console.log (list);
+showList();
 
-``
+// const STATUS = {
+//   TO_DO: 'To Do',
+//   IN_PROGRESS: 'In Progress',
+//   DONE: 'Done',
+// }
+//
+// const DEFAULT_STATUS = STATUS.TO_DO;
+
+// const list = {
+//  "create a new practice task": STATUS.TO_DO,
+//  "make a bed": STATUS.TO_DO,
+//  "write a post": STATUS.IN_PROGRESS,
+// }
+//
+// function deleteTask(task) {
+//   if (task in list) {
+//     delete list[task];
+//   }
+// }
+//
+// function addTask(task) {
+//   list[task] = DEFAULT_STATUS;
+// }
+//
+// function changeStatus(task, status) {
+//   if (task in list) {
+//     list[task] = status;
+//   }
+// }
+//
+// function showList() {
+//   const tasks = {
+//     [STATUS.TO_DO]: '',
+//     [STATUS.IN_PROGRESS]: '',
+//     [STATUS.DONE]: '',
+//   }
+//
+//   for (let task in list) {
+//     tasks[list[task]] += ` '${task}'` + '\n';
+//   }
+//
+//   console.log('To do: \n' + (tasks[STATUS.TO_DO] || ' -') +
+//             '\nIn progress: \n' + (tasks[STATUS.IN_PROGRESS] || ' -') +
+//             '\nDone: \n' + (tasks[STATUS.DONE] || ' -'));
+// }
+//
+//
+// // addTask('have a walk') ;
+// // deleteTask('make a bed');
+// // changeStatus('write a post', 'Done');
+// showList();
+// // console.log (list);
+//
 
